7dd7fe16e159d195f243073f82a606ad7ac7b034
skharbash <v-skharbash@expediagroup.com>
2021-04-09 19:33:56 +0530

    000000

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const github = require('@actions/github');
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
+  const nameToGreet = core.getInput('numOctocats');
+  const nameToGreettwo = core.getInput('octocatEyeColor');
   console.log(`Hello ${nameToGreet}!`);
-  console.log(`Hello2 ${nameToGreet2}!`);
+  console.log(`Hellotwo ${nameToGreettwo}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
-  core.setOutput("timetwo", '92039430940340390249');
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
